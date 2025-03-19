792feabe7a98fba93fd5b8e6ad502166ff556a73
cagcal <c.calguner@gmail.com>
2025-03-19 19:56:03 +0100

    Create app.py

---
+++ app.py
@@ -0,0 +1,9 @@
+import streamlit as st
+
+# App Title
+st.title("🎤 K-Pop Singers Dashboard")
+st.write("This app provides information about K-Pop singers.")
+
+# Button Example
+if st.button("Say Hello!"):
+    st.write("Hello! We are building a Streamlit app! 🎉")
